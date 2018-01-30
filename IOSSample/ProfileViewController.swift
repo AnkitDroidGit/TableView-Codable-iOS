ca1f9c4cc33d17a8e8294b1ebd5f5df2a21c5a81
Ankit Kumar <ankit@ankit-kumar-macbook.local>
2018-01-30 13:29:48 +0800

    rafactored

---
+++ IOSSample/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  IOSSample
+//
+//  Created by Ankit Kumar on 30/01/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
